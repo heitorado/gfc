c49244ab47ecbeb1b8491a348ba0b5486b00d347
Heitorado <hscs@cin.ufpe.br>
2018-12-06 13:56:57 -0300

    Correções no arquivo app.module para incluir os componentes e serviços

--- gfc-gui/src/app/app.module.ts
+++ gfc-gui/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users.component';
+import { WishlistComponent } from './wishlist.component';
+
+import { PossessionsService } from './possessions.service';
+import { UserService } from './user.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PossessionsService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
