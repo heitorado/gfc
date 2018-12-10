a6d86578f75814079a1ddd8fcf521a31a69e1641
Saulo Guilhermino Ferreira Lima <sgfl@ug4c29.windows.cin.ufpe.br>
2018-12-10 15:18:04 -0300

    testes iniciados, implementado checkbox variável na lista de contas

--- tests-acceptance/features/commonExpenses.feature
+++ tests-acceptance/features/commonExpenses.feature
@@ -4,31 +4,29 @@ Feature: As a house member
 
 Scenario: Adding a paid bill
 Given I am at the "Common Expenses" page
-And I do not see a bill named “Conta de Internet”
-When I create a bill named “Conta de Internet” with the value “150,00”
-And I mark the paid checkbox
-And I reload the page
-Then I can see a paid bill named “Conta de Internet” with the value “150,00”
-And the Safe Box has a debit of “150,00”
+And I can not see a bill named “Conta de Internet”
+When I create a paid bill named “Conta de Internet” with the value “150”
+Then I can see a paid bill named “Conta de Internet” with the value “150”
+And the Safe Box has a debit of “150”
 
 Scenario: Changing the cost of an light bill expired
 Given I am at the "Common Expenses" page
-And I see an expired bill with the value “32,00”
+And I can see an expired bill with the value “32”
 When I click over the bill
-And I change the value to “35,00”
+And I change the value to “35”
 And I reload the page
-Then I can see an expired bill with the value “35,00”
+Then I can see an expired bill with the value “35”
 
 Scenario: Delaying an expiration of a bill that is alerted as “expiration date soon” 
 Given I am at the "Common Expenses" page
-And I see an “expiration date today” alert to the bill “Conta de telefone” 
+And I can see an “expiration date today” alert to the bill “Conta de telefone” 
 When I daley the expiration date of the bill “Conta de telefone” in “14” days from now
 And I reload the page
 Then I do not see an “expiration date today” alert to the bill “Conta de telefone”
 
 Scenario: Change a bill’s status from “paid” to “not paid”
 Given I am at the "Common Expenses" page
-And I see a paid bill named “Conta de luz” with the value “60,00”
+And I can see a paid bill named “Conta de luz” with the value “60”
 When change the bill’s status to “not paid”
-Then I can see a not paid bill named “Conta de luz” with the value “60,00”
-And the Safe Box has a credit of “60,00”
+Then I can see a not paid bill named “Conta de luz” with the value “60”
+And the Safe Box has a credit of “60”
---
+++ tests-acceptance/stepdefinitions/bills.ts
@@ -0,0 +1,46 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
+
+let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
+let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+
+let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^I am at the "Common Expenses" page$/, async (title) => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('GfcGui');
+        await $("a[name='bills']").click();
+    })
+
+    Given(/^I can not see a bill named “([^\"]*)” $/, async (name) => {
+        var allbills : ElementArrayFinder = element.all(by.name('nomelist'));
+        await allbills;
+        var samename = allbills.filter(elem =>
+                                      elem.getText().then(text => text === name));
+        await samename;
+        await samename.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+
+    When(/^I create a paid bill named “([^\"]*)” with the value “(\d*)”$/, async (name, value) => {
+        await $("input[name='namebox']").sendKeys(<string> name);
+        await $("input[name='valuebox']").sendKeys(<number> value);
+        await element(by.name('statusbox')).click();
+        await element(by.buttonText('Adicionar')).click();
+    });
+    
+    When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+        await $("input[name='namebox']").sendKeys(<string> name);
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await element(by.buttonText('Adicionar')).click();
+    });
+
+    Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+})
